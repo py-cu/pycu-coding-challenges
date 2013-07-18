4747773ec1b36aef3befbb836e8e75be8ad6a999
me <me@me>
2013-07-18 17:12:49 -0500

    initial commit with simple kata/test, .gitignore

---
+++ kata1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+""" 
+Given a list of integers from 0 through 10 (in order), return
+just the evens, also in a list.  
+
+Do this in three different ways -- fill in each function below. 
+
+Run the tests with 
+$ python kata1_test.py
+"""
+
+def evens_list1(the_list):
+    """ Method 1: slicing """
+    return the_list[::2]
+
+def evens_list2(the_list):
+    """ Method 2: list comprehension """
+    return [i for i in the_list if i % 2 == 0]
+
+def evens_list3(the_list):
+    """ Method 2: filter + lambda """
+    return filter(lambda x: x % 2 == 0, the_list)
